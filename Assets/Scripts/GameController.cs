079b772e52c5b8b422a0f53f3abdffce70fab887
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2022-05-06 18:16:24 -0300

    feat: starting the scripts that create the gameplay.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private PlayerController playerOne;
+    private PlayerController playerTwo;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        playerOne = new PlayerController();
+        playerOne
+            .SetScore(0, 0)
+            .SetScore(0, 1)
+            .SetScore(1, 0);
+
+        bool hasWon = playerOne.CheckHasWon();
+
+        Debug.Log("Player One has won: " + hasWon);
+        playerOne.DisplayGrid();
+
+        playerOne.ResetScore();
+        playerOne
+            .SetScore(0, 0)
+            .SetScore(0, 1)
+            .SetScore(0, 2);
+
+        hasWon = playerOne.CheckHasWon();
+
+        Debug.Log("Player One has won: " + hasWon);
+        playerOne.DisplayGrid();
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool[,] scoreArray = new bool[3, 3];
+    private int rowCount = 3;
+    private int columnCount = 3;
+
+    public void ResetScore()
+    {
+        for (int i = 0; i < rowCount; i++)
+        {
+            for (int j = 0; j < columnCount; j++)
+            {
+                SetScore(i, j, false);
+            }
+        }
+    }
+    public PlayerController SetScore(int rowPosition, int columnPosition, bool valueToSet = true)
+    {
+        rowPosition = rowPosition >= rowCount? rowCount - 1 : rowPosition;
+        columnPosition = columnPosition >= columnCount? rowCount - 1 : columnPosition;
+
+        scoreArray[rowPosition, columnPosition] = valueToSet;
+
+        return this;
+    }
+    public bool CheckHasWon()
+    {
+        bool hasWon = false;
+        int lineScoreCount = 0;
+        bool hasScore = false;
+
+        //check row
+        for (int i = 0; i < rowCount; i++)
+        {
+            for (int j = 0; j < columnCount; j++)
+            {
+                hasScore = scoreArray[i, j] == true;
+                if (hasScore)
+                    lineScoreCount++;
+
+                if (lineScoreCount >= columnCount)
+                {
+                    hasWon = true;
+                    break;
+                }
+            }
+
+            lineScoreCount = 0;
+            hasScore = false;
+        }
+
+        return hasWon;
+    }
+
+    public void DisplayGrid()
+    {
+        string text = "";
+        for (int i = 0; i < rowCount; i++)
+        {
+            for (int j = 0; j < columnCount; j++)
+            {
+                text += scoreArray[i, j].ToString() + "|";
+            }
+            text += "\n";
+        }
+        Debug.Log(text);
+    }
+}
