254e53b15e3ebd0cc96107321587a63470aae38e
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2022-05-13 16:08:20 -0300

    feat: showing in the UI the player turn and that the match has ended.

---
+++ Assets/Scripts/MatchController/MatchController_OnEndMatch_UpdateUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class MatchController_OnEndMatch_UpdateUI : MonoBehaviour, IMatchController_OnEndMatch
+{
+    [SerializeField] private TextMeshProUGUI descriptionText;
+
+    public void OnEndMatch()
+    {
+        int shownID = MatchController.ActivePlayer.PlayerID + 1;
+        string text = "";
+
+        switch (MatchController.CurrentMatchStatus)
+        {
+            case MatchController.MatchStatus.PLAYING:
+                break;
+            case MatchController.MatchStatus.WON:
+                text = "Player " + shownID + " is the winner!!!";
+                break;
+            case MatchController.MatchStatus.DRAW:
+                text = "The game is a draw!";
+                break;
+            case MatchController.MatchStatus.GIVE_UP:
+                break;
+            default:
+                break;
+        }
+
+        descriptionText.text = text;
+    }
+}
---
+++ Assets/Scripts/MatchController/MatchController_OnStartTurn_UpdateUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class MatchController_OnStartTurn_UpdateUI : MonoBehaviour, IMatchController_OnStartTurn
+{
+    [SerializeField] private TextMeshProUGUI descriptionText;
+
+    public void OnStartTurn()
+    {
+        int shownID = MatchController.ActivePlayer.PlayerID + 1;
+        string text = "Player " + shownID.ToString() + " turn";
+
+        descriptionText.text = text;
+    }
+}
