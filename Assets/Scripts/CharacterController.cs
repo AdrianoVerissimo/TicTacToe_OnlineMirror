dcb399dff2c677d5e0b75a9a9d3f2bc19fc17528
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2022-05-17 17:59:18 -0300

    feat: sharing the players that are connected in the match.

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -50,14 +50,58 @@ public class CharacterController : NetworkBehaviour
         if (!isLocalPlayer)
             return;
 
-        GameObject battleController_NetworkInstance = Instantiate(battleController_NetworkPrefab);
+        Network_CreateBattleControllerNetwork();
+
+        Network_RegisterPlayerOnMatch(netIdentity);
+    }
+
+    public void Network_CreateBattleControllerNetwork()
+    {
+        if (isServer)
+        {
+            Cmd_CreateBattleControllerNetwork();
+        }
+    }
+    private void CreateBattleControllerNetwork()
+    {
         if (BattleController_Network.Instance == null)
         {
+            GameObject battleController_NetworkInstance = Instantiate(battleController_NetworkPrefab);
             NetworkServer.Spawn(battleController_NetworkInstance);
         }
     }
 
-    
+    [Command]
+    private void Cmd_CreateBattleControllerNetwork()
+    {
+        CreateBattleControllerNetwork();
+    }
+
+
+
+    public void Network_RegisterPlayerOnMatch(NetworkIdentity playerNetworkIdentity)
+    {
+        if (isServer)
+            RegisterPlayerOnMatch(playerNetworkIdentity);
+        else
+            Cmd_RegisterPlayerOnMatch(playerNetworkIdentity);
+    }
+    public void RegisterPlayerOnMatch(NetworkIdentity playerNetworkIdentity)
+    {
+        CharacterController player = playerNetworkIdentity.GetComponent<CharacterController>();
+
+        if (BattleController_Network.Instance.playerOne == null)
+            BattleController_Network.Instance.playerOne = player;
+        else if (BattleController_Network.Instance.playerTwo == null)
+            BattleController_Network.Instance.playerTwo = player;
+    }
+
+    [Command(channel = 0, requiresAuthority = false)]
+    private void Cmd_RegisterPlayerOnMatch(NetworkIdentity playerNetworkIdentity)
+    {
+        Debug.Log("Cmd_RegisterPlayerOnMatch");
+        RegisterPlayerOnMatch(playerNetworkIdentity);
+    }
 
     #endregion
 }
--- Assets/Scripts/MatchController/BattleController_Network.cs
+++ Assets/Scripts/MatchController/BattleController_Network.cs
@@ -25,36 +25,7 @@ public class BattleController_Network : NetworkBehaviour
         networkManager = NetworkManager.singleton;
     }
 
-    public void Network_RegisterPlayerOnMatch(NetworkIdentity playerNetworkIdentity)
-    {
-        if (isServer)
-            Rpc_RegisterPlayerOnMatch(playerNetworkIdentity);
-        else
-            Cmd_RegisterPlayerOnMatch(playerNetworkIdentity);
-    }
-    public void RegisterPlayerOnMatch(NetworkIdentity playerNetworkIdentity)
-    {
-        CharacterController player = playerNetworkIdentity.GetComponent<CharacterController>();
-
-        if (playerOne == null)
-            playerOne = player;
-        else if (playerTwo == null)
-            playerTwo = player;
-    }
-    [Command]
-    private void Cmd_RegisterPlayerOnMatch(NetworkIdentity playerNetworkIdentity)
-    {
-        Debug.Log("Cmd_RegisterPlayerOnMatch");
-        Rpc_RegisterPlayerOnMatch(playerNetworkIdentity);
-    }
-
-    [ClientRpc]
-    private void Rpc_RegisterPlayerOnMatch(NetworkIdentity playerNetworkIdentity)
-    {
-        Debug.Log("Rpc_RegisterPlayerOnMatch");
-        RegisterPlayerOnMatch(playerNetworkIdentity);
-        ShowPlayers();
-    }
+    
 
     public void ShowPlayers()
     {
