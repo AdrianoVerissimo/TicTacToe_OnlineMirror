d7cc2671ee47e7f6d917ce8f7b76ff2b48f2f8c1
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2022-05-26 16:11:17 -0300

    feat: making the navigation of the lobby menu.

---
+++ Assets/Scripts/Lobby/LobbyController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LobbyController : MonoBehaviour
+{
+    [SerializeField] private LobbyScreen enterNameScreen; 
+    [SerializeField] private LobbyScreen connectionScreen; 
+
+    public void ShowEnterNameScreen()
+    {
+        enterNameScreen.gameObject.SetActive(true);
+        connectionScreen.gameObject.SetActive(false);
+    }
+    public void ShowConnectionScreen()
+    {
+        connectionScreen.gameObject.SetActive(true);
+        enterNameScreen.gameObject.SetActive(false);
+    }
+}
